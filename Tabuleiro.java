71f4b28ccb21b2c544d7903c04aa4631669cca16
Ana Luiza Mattia de Lima <91649366+ana-luiza-lima@users.noreply.github.com>
2025-12-15 22:38:48 +0000

    alterações

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -3,7 +3,6 @@ import java.util.Random;
 
 public class Tabuleiro {
 
-    static final Random random= new Random();
     static final char AGUA = '.';  // Estamos definindo um constante para preencher a matriz
     static final int[] TAMANHO_NAVIO = {5,4,3,2,1}; // definimos um array para armazenar o valor de tamanho de cada navio
     static final char[] NAVIO = {'P','E','C','S','A'}; // definimos o nome de cada navio respectivo com o tamanho.
@@ -11,20 +10,58 @@ public class Tabuleiro {
 
 
     private static char[][] geraTabuleiro(){
-        char[][] MATRIZ = new char [TAMANHO][TAMANHO]; // Criamos a matriz e definimos o tamnho dela como 10 x 10
+        char[][] tabuleiro = new char [TAMANHO][TAMANHO]; // Criamos a matriz e definimos o tamnho dela como 10 x 10
+
         for (int i = 0; i < TAMANHO; i++) {
-            for ( int b =0;b<TAMANHO; b++){
-                MATRIZ [i][b] = AGUA;                  //definimos tudo com agua
+            for ( int j =0; j < TAMANHO; j++){
+                tabuleiro [i][j] = AGUA;                  //definimos tudo com agua, deixando vazio
             }    
         }
-        return MATRIZ;                                 //retornamos o valor para o metodo estatico como sendo a matriz com agua
 
+        Random random= new Random();
+
+
+        return tabuleiro;                                 //retornamos o valor para o metodo estatico como sendo a matriz com agua
     }
+
+    private static void posicionaNavio(char[][] tabuleiro, char simbolo, int tamanho, Random random) {
+        boolean posicionado = false; // verifica se o navio foi posicionado, inicializa em 'false' pra indicar que ele não ta posicionado
+
+        while(!posicionado) { // loop de posicionamento
+            boolean horizontal = random.nextBoolean(); // booleano pra definir se será horizontalmente ou verticalmente, sendo true pra horizontal e false pra vertical
+            int linha = random.nextInt(TAMANHO); // 
+            int coluna = random.nextInt(TAMANHO);
+
+            if (verificaPosicao(tabuleiro, linha, coluna, tamanho, horizontal)) {
+                if (horizontal) { // se foi denifido horizontal, ele entra aqui
+                    for (int j = coluna; j < coluna + tamanho; j++) {
+                        tabuleiro[linha][j] = simbolo;
+                    }
+                } else { // se não, ele faz o loop para ser na vertical
+                    for (int i = linha; i < linha + tamanho; i++) {
+                        tabuleiro[i][coluna] = simbolo;
+                    }
+                }
+                posicionado = true;
+            }
+        }
+    }
+
+    private static boolean verificaPosicao(char[][] tabuleiro, int linha, int coluna, int tamanho, boolean horizontal){ // método pra verificar se é possível colocar o navio naquela posição
+        if (horizontal){
+            if (coluna + tamanho > TAMANHO) { //Verifica o tamanho
+                return false;
+            }
+
+
+        }
+    }
+
 
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in); 
 
-        
+
 
     
    
